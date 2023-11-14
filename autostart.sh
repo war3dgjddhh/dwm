f280d79f0e8b5f681db0e864081a2d4b7783b1fe
ylyhappy-hello <2778335106@qq.com>
2023-11-14 22:31:14 +0800

    fit nixos
    rm source ~/.profile
    update $DEM to ~/dwm
    add fcitx5 start
    add flameshot start
    add feh start

--- autostart.sh
+++ autostart.sh
@@ -3,8 +3,6 @@
 # 搭配 https://github.com/yaocccc/scripts 仓库使用 目录位置 ~/scripts
 # 部分配置文件在 ~/scripts/config 目录下
 
-source ~/.profile
-
 settings() {
     [ $1 ] && sleep $1
     xset -b                                   # 关闭蜂鸣器
@@ -14,25 +12,24 @@ settings() {
 
 daemons() {
     [ $1 ] && sleep $1
-    $DWM/statusbar/statusbar.sh cron &        # 开启状态栏定时更新
-   #xss-lock -- ~/scripts/blurlock.sh &       # 开启自动锁屏程序
-   #fcitx5 &                                  # 开启输入法
-   #lemonade server &                         # 开启lemonade 远程剪切板支持
-   #flameshot &                               # 截图要跑一个程序在后台 不然无法将截图保存到剪贴板
+    ~/dwm/statusbar/statusbar.sh cron &        # 开启状态栏定时更新
+    #xss-lock -- ~/scripts/blurlock.sh &       # 开启自动锁屏程序
+    fcitx5 &                                  # 开启输入法
+    #lemonade server &                         # 开启lemonade 远程剪切板支持
+    flameshot &                               # 截图要跑一个程序在后台 不然无法将截图保存到剪贴板
     dunst -conf ~/scripts/config/dunst.conf & # 开启通知server
-    picom --experimental-backends --config ~/scripts/config/picom.conf >> /dev/null 2>&1 & # 开启picom
+    #picom --experimental-backends --config ~/scripts/config/picom.conf >> /dev/null 2>&1 & # 开启picom
 }
 
 cron() {
     [ $1 ] && sleep $1
-    #let i=10
-    #while true; do
-       #[ $((i % 10)) -eq 0 ] && ~/scripts/set_screen.sh check # 每10秒检查显示器状态 以此自动设置显示器
-       #[ $((i % 300)) -eq 0 ] && feh --randomize --bg-fill ~/Pictures/wallpaper/*.png # 每300秒更新壁纸
-    #    sleep 10; let i+=10
-    #done
+    let i=10
+    while true; do
+       [ $((i % 300)) -eq 0 ] && feh --randomize --bg-fill ~/wallpaper/*.png # 每300秒更新壁纸
+        sleep 10; let i+=10
+    done
 }
 
 settings 1 &                                  # 初始化设置项
 daemons 3 &                                   # 后台程序项
-#cron 5 &                                      # 定时任务项
+cron 5 &                                      # 定时任务项
